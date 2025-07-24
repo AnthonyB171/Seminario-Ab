c07e8f305faa2ba2c50c1a95caa483391c0aeb3d
Anthonyb171 <anthony.bacah@pca.edu.co>
2025-07-23 23:01:31 -0500

    avances

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [CommonModule, FormsModule, IonicModule, ReactiveFormsModule]
+})
+export class LoginPage implements OnInit {
+
+  loginForm: FormGroup;
+
+  validation_messages = {
+    email: [
+      { type: 'required', message: 'El correo es obligatorio' },
+      { type: 'email', message: 'El correo no es válido' }
+    ],
+    password: [
+      { type: 'required', message: 'La contraseña es obligatoria' },
+      { type: 'minlength', message: 'La contraseña debe tener al menos 6 caracteres' }
+    ]
+  };
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    });
+  }
+
+  ngOnInit() {}
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Formulario válido:', this.loginForm.value);
+      // Aquí puedes realizar el login (API, navegación, etc.)
+    } else {
+      console.log('Formulario inválido');
+      this.loginForm.markAllAsTouched();
+    }
+  }
+}
